d401298b175331ebbbb24a28feb5b9954918820b
waltervrossem <walter_v_rossem@hotmail.com>
2024-10-18 18:36:39 +0200

    Corrected order in assertions, use path to __file__ to find test_data.

--- tests/test_inlists/test_inlists.py
+++ tests/test_inlists/test_inlists.py
@@ -9,8 +9,9 @@ from f90nml import Namelist
 from wssss.inlists import inlists as inl
 
 test_data = '../data/inlists'
+test_data = os.path.join(os.path.dirname(__file__), '..', 'data', 'inlists')
 
-class TestFunctions(unittest.TestCase):
+class TestInlists(unittest.TestCase):
     def setUp(self):
         inlist = inl.evaluate_inlist(os.path.join(test_data, 'inlist'))
         in1 = inl.evaluate_inlist(os.path.join(test_data, 'inlist_1'))
@@ -30,24 +31,24 @@ class TestFunctions(unittest.TestCase):
 
 
     def test_evaluate_inlist(self):
-        self.assertDictEqual(self.inlist, self.correct)
+        self.assertDictEqual(self.correct, self.inlist)
 
 
     def test_evaluate_inlist_str(self):
         with open(os.path.join(test_data, 'inlist'), 'r') as handle:
             inlist_str = handle.read()
         inlist = inl.evaluate_inlist_str(inlist_str, test_data)
-        self.assertDictEqual(inlist, self.correct)
+        self.assertDictEqual(self.correct, inlist)
 
 
     def test_inlist_diff(self):
         self.assertRaises(ValueError, inl.inlist_diff, self.inlist, self.inlist)
-        self.assertDictEqual(inl.inlist_diff(self.inlist['controls'], self.inlist['controls']),
-                             {'same':dict(self.inlist['controls']), 'changed':{}})
+        self.assertDictEqual({'same':dict(self.inlist['controls']), 'changed':{}},
+                             inl.inlist_diff(self.inlist['controls'], self.inlist['controls']),)
 
         diff_in1_in3 = {'same': {},
                         'changed': {'initial_mass': (1.0, 3.0), 'initial_z': (0.01, None)}}
-        self.assertDictEqual(inl.inlist_diff(self.sub_inlists[0]['controls'], self.sub_inlists[2]['controls']), diff_in1_in3)
+        self.assertDictEqual(diff_in1_in3, inl.inlist_diff(self.sub_inlists[0]['controls'], self.sub_inlists[2]['controls']))
 
 
     def test_compare_inlist(self):
@@ -55,16 +56,17 @@ class TestFunctions(unittest.TestCase):
         sys.stdout = capturedOutput  # Redirect stdout.
         inl.compare_inlist(os.path.join(test_data, 'inlist_3'), os.path.join(test_data, 'inlist_1'))
         sys.stdout = sys.__stdout__ # Reset redirect.
-        self.assertEqual(capturedOutput.getvalue(), ("left : ../data/inlists/inlist_3\n"
-                                                     "right: ../data/inlists/inlist_1\n"
-                                                     "\n"
-                                                     "####### star_job differences #######\n"
-                                                     "chem_isotopes_filename: ('isotopes.data', None)\n"
-                                                     "\n"
-                                                     "####### controls differences #######\n"
-                                                     "initial_mass: (3.0, 1.0)\n"
-                                                     "initial_z: (None, 0.01)\n"
-                                                     ))
+        expected = ("left : /home/walter/Github/wssss/tests/test_inlists/../data/inlists/inlist_3\n"
+                    "right: /home/walter/Github/wssss/tests/test_inlists/../data/inlists/inlist_1\n"
+                    "\n"
+                    "####### star_job differences #######\n"
+                    "chem_isotopes_filename: ('isotopes.data', None)\n"
+                    "\n"
+                    "####### controls differences #######\n"
+                    "initial_mass: (3.0, 1.0)\n"
+                    "initial_z: (None, 0.01)\n"
+                    )
+        self.assertEqual(expected, capturedOutput.getvalue())
 
     def test_round_trip(self):
         self.inlist.write(os.path.join(test_data, 'write_inlist'), force=True)
