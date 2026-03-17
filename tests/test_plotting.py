95382c6cc7613d29be480cc044267f659ef9513e
waltervrossem <walter_v_rossem@hotmail.com>
2026-03-17 17:13:12 +0100

    test_plotting.py: Close figures after creation. Also add hist=None for period_spacing and echelle

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -25,6 +25,7 @@ class TestFunctions(unittest.TestCase):
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_vhrd(hist)
+        plt.close('all')
         
 
     def test_make_hrd(self):
@@ -32,105 +33,122 @@ class TestFunctions(unittest.TestCase):
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_hrd(hist)
-        
+        plt.close('all')
+
 
     def test_make_propagation(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_propagation(profs[-1], hist)
-        
+        plt.close('all')
+
 
     def test_make_propagation2(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_propagation2(profs[-1], hist)
-        
+        plt.close('all')
+
 
     def test_make_echelle(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_echelle(gss[-1], hist)
-        
+        f, ax = pl.make_echelle(gss[-1], None, delta_nu='median')
+        plt.close('all')
+
 
     def test_make_inertia(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_inertia(gss[-1])
-        
+        plt.close('all')
+
 
     def test_make_resolutions(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax, (counts, edges_x, edges_y) = pl.make_resolutions(profs)
-        
+        plt.close('all')
+
 
     def test_make_hrd_profiles(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_hrd_profiles(hist)
-        
+        plt.close('all')
+
 
     def test_make_hrd_models(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_hrd_models(hist)
-        
+        plt.close('all')
+
 
     def test_make_age_nu(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_age_nu(hist, gss)
-        
+        plt.close('all')
+
 
     def test_make_mesa_gyre_delta_nu(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_mesa_gyre_delta_nu(hist, gss)
-        
+        plt.close('all')
+
 
     def test_make_composition(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_composition(profs[-1])
-        
+        plt.close('all')
+
 
     def test_make_gradients(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_gradients(profs[-1])
-        
+        plt.close('all')
+
 
     def test_make_period_spacing(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_period_spacing(gss[-1], hist)
-        
+        f, ax = pl.make_period_spacing(gss[-1], None)
+        plt.close('all')
+
 
     def test_make_structural(self):
         hist = self.hist
         profs = self.profs
         gss = self.gss
         f, ax = pl.make_structural(profs[-1])
-        
+        plt.close('all')
+
 
     # def test_make_eos(self):
     #     hist = self.hist
     #     profs = self.profs
     #     gss = self.gss
     #     f, ax = pl.make_eos(profs[-1])
-        
+    #     plt.close('all')
+
 
     def test_make_eigenfunc_compare(self):
         hist = self.hist
@@ -138,7 +156,8 @@ class TestFunctions(unittest.TestCase):
         gss = self.gss
         gef = ld.load_modes_from_profile(profs[-1])
         f, ax = pl.make_eigenfunc_compare(gss[-1], gef, profs[-1], hist)
-        
+        plt.close('all')
+
 
     def test_make_kipp(self):
         hist = self.hist
@@ -152,3 +171,4 @@ class TestFunctions(unittest.TestCase):
         kwargs_colors = {'norm':norm}
         f, ax = pl.make_kipp(hist, profs, caxis='brunt_N2', kwargs_profile_color=kwargs_colors)
         f, ax = pl.make_kipp(hist, profs, caxis='brunt_N2', norm=norm, kwargs_profile_color=kwargs_colors)
+        plt.close('all')
